5986c17210380586b13d786afc7a53e4f51aa9b2
Gabriel Fok <gfok@cpp.com>
2021-09-22 17:09:37 -0700

    Uploaded initial travelexpenses.h file

---
+++ travelexpenses.h
@@ -0,0 +1,11 @@
+#ifndef TRAVELEXPENSES_H
+#define TRAVELEXPENSES_H
+
+double drivingExpense(double miles);
+
+double maxExpensesParking(int parkingDays);
+double maxExpensesTaxi(int taxiDays);
+double maxExpensesHotel(int nights);
+double maxExpensesMeal(double departureTime, double arrivalTime);
+
+#endif
