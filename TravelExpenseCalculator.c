86a4181790ee848567bae49d39f467931aaee4e8
norivera <89235780+norivera@users.noreply.github.com>
2021-09-26 12:35:12 -0700

    create the function to get the days

--- TravelExpenseCalculator.c
+++ TravelExpenseCalculator.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+double setTotalDays(double days)
+{
+    printf("How many days were spent on the trip?\n");
+    scanf("%lf", &days);
+    while ( days < 1 )
+    {
+        printf("Please enter a number greater than 1\n");
+        scanf("%lf", &days);
+    }
+    printf("Totals days spent:%.2lf", days);
+    return days;
+}
+
